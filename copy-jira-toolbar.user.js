6f9b297988bc96f4fcc96968384d07104035f7b0
garanmoon <garan@dable.io>
2025-06-26 12:36:56 +0900

    require 버저닝

--- copy-jira-toolbar.user.js
+++ copy-jira-toolbar.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         Copy Jira Toolbar
 // @namespace    http://tampermonkey.net/
-// @version      25062503
+// @version      25062601
 // @description  Jira 내용을 복사합니다.
 // @author       garan-dable
 // @match        https://teamdable.atlassian.net/browse/*
-// @updateURL    https://cdn.jsdelivr.net/gh/garan-dable/copy-jira-toolbar@latest/copy-jira-toolbar.user.js
-// @downloadURL  https://cdn.jsdelivr.net/gh/garan-dable/copy-jira-toolbar@latest/copy-jira-toolbar.user.js
-// @require      https://cdn.jsdelivr.net/gh/garan-dable/copy-jira-toolbar/main.js
-// @require      https://cdn.jsdelivr.net/gh/garan-dable/copy-jira-toolbar/turndown.js
+// @updateURL    https://cdn.jsdelivr.net/gh/garan-dable/copy-jira-toolbar@25062601/copy-jira-toolbar.user.js
+// @downloadURL  https://cdn.jsdelivr.net/gh/garan-dable/copy-jira-toolbar@25062601/copy-jira-toolbar.user.js
+// @require      https://cdn.jsdelivr.net/gh/garan-dable/copy-jira-toolbar@25062601/main.js
+// @require      https://cdn.jsdelivr.net/gh/garan-dable/copy-jira-toolbar@25062601/turndown.js
 // @grant        none
 // ==/UserScript==
